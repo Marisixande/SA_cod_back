fe7190c221524aa5c2142eb03f77410677aaba1a
A.Macedo <mariaclaramacedosouza71@gmail.com>
2026-03-30 15:53:12 -0300

    Atualização service.js

--- src/services/funcionarios.service.js
+++ src/services/funcionarios.service.js
@@ -4,7 +4,11 @@ const funcionarios = [
   {id: 2, name: "Pietra", age: "43", codigo: "050709"},
   {id: 3, name: "Joanna", age: "28", codigo: "150308"},
   {id: 4, name: "Caua", age: "35", codigo: "290918"},
-  {id:5, name:"Marinho", age: "23", codigo: "280405"}
+  {id: 5, name: "Marinho", age: "23", codigo: "280405"}, 
+  {id: 6, name: "Teodoro", age: "19", codigo: "241108"},
+  {id: 7, name: "Pyetro", age: "22", codigo: "010809"},
+  {id: 8, name: "Lizzy", age: "25", codigo: "210809"},
+  {id: 9, name: "Anita", age: "21", codigo: "011210"}, 
 ]
 
 class FuncionariosService {
